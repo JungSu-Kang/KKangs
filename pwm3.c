21244b63d46066c0826cd56db3dd9b1c9736fb1d
JungSu Kang <js.kkangs@gmail.com>
2024-07-02 17:51:34 +0900

    update3

---
+++ pwm3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+
+#define wPi_PWM1 23
+int main()
+{
+	wiringPiSetup();
+	pinMode(wPi_PWM1, PWM_OUTPUT);
+	pwmWrite(wPi_PWM1, 500);
+	
+	return 0;
+}
+// H/W pwm mode control
