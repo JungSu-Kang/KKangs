f70f4413740987e7ec2e3feaf13c359a9d47ae45
JungSu Kang <js.kkangs@gmail.com>
2024-07-08 14:15:20 +0900

    Update0708

--- pwm2.c
+++ pwm2.c
@@ -2,17 +2,17 @@
 #include <wiringPi.h>
 #include <softPwm.h>
 
-#define wPi_Pin 22		// 변수화 or 정의(define) 해 놓으면 변경 상황 발생시 관리가 용이함.
+#define wPi_Pin 7		// 변수화 or 정의(define) 해 놓으면 변경 상황 발생시 관리가 용이함.
 int main(int n, char * s[])
 {
 	if(n<2)
 	{
 		printf("usage : pwm2 [duty_rate(%%)] [[period]]\n\n");
-		printf("        period = 10 ms if empty\n\n");
-		return 1;
+		printf("default value : period = 10 ms, DR = 50%%\n\n");
+		// return 1;
 	}
-	int dr, drh, period = 10;
-	sscanf(s[1], "%d", &dr);	// scanf -> 콘솔함수
+	int dr = 50, drh, period = 10;
+	if (n>1) sscanf(s[1], "%d", &dr);	// scanf -> 콘솔함수
 		// sscanf -> scanf의 파생 함수로 string scanf의 뜻으로 문자열에서 입력 받음.
 		// agument가 추가 됨"s[1]"에서 "%d" 값을 &dr에 넣어라
 	if(n > 2) sscanf(s[2], "%d", &period);
---
+++ uSonic2ver.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softTone.h>
+#include <softPwm.h>
+
+#define TRIG 8
+#define ECHO 9
+#define SUND 7
+
+double Dist()
+{
+	// Trigger signal
+		digitalWrite(TRIG, 1);
+		delayMicroseconds(10);		// raspberry Pi에서는 us 단위를 제공함으로 사용 가능함
+		digitalWrite(TRIG, 0);
+		delayMicroseconds(200);		// 200 x 0.34mm = 68mm는 무시 한다는 의미
+		
+		//while(digitalRead(ECHO) != 0);		// Wait for burst Start
+		// Wait for Echo signal //전원 인가 후 최초에만 유효한 명령어(없어도 되는 명령어)
+		
+		while(digitalRead(ECHO) != 1);		// Wait for Echo High
+		// burst 신호 이후 rising edge 될때까지 (t1 time count start)
+		int t1 = micros();					// Get start time(in micros-second)
+		while(digitalRead(ECHO) != 0);		// Wait for Echo Low
+		// high 신호 이후 falling edge 될때까 (t2 time count)
+		int t2 = micros();					// Get end time
+		//double dist = (t2 - t1) * (340 / 1000000 / 2 * 100);
+		// 340m/s(음속) / 1000000s(us로 변환) / 2(왕복이므로 거리는 절반) * 100(cm로 변환)
+		// double dist = (t2 - t1) * 0.017; 	// 거리 계산 간소화 식 by cm
+		return (t2 - t1) * 0.017;
+}
+int main(int argc, char **argv)
+{
+	wiringPiSetup(); 			// Pin numbering method : wPi
+	pinMode(TRIG, OUTPUT);
+	pinMode(ECHO, INPUT);
+	pinMode(SUND, OUTPUT);
+	
+	for(;;)
+	{
+		printf("Distance : %f(cm) \n", Dist());
+		delay(1000);		//기본 delay는 ms 단위 1000
+		
+		if (Dist() <
+	}
+	
+	return 0;
+}
